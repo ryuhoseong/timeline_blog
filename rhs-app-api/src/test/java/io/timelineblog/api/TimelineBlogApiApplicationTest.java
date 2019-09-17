946b0cd7081c9f2f29abdfccc30016e71f9aeac8
rhs <ar.ryuhs@gmail.com>
2019-09-18 00:03:27 +0900

    master

---
+++ rhs-app-api/src/test/java/io/timelineblog/api/TimelineBlogApiApplicationTest.java
@@ -0,0 +1,18 @@
+package io.timelineblog.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@ActiveProfiles("dev")
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimelineBlogApiApplicationTest {
+	
+	@Test
+	public void contextLoads() {
+	}
+
+}
