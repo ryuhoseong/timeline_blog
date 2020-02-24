987e8ea0c982ca84eb965449346688cd8c169223
rhs <ar.ryuhs@gmail.com>
2020-02-24 23:51:56 +0900

    TimelineKeyword domain 추가

--- rhs-timeline-api/src/main/java/io/toy/timeline/domain/TimelineKeyword.java
+++ rhs-timeline-api/src/main/java/io/toy/timeline/domain/TimelineKeyword.java
@@ -1,46 +1,23 @@
 package io.toy.timeline.domain;
 
-import io.toy.timeline.domain.enumeration.Topic;
-import io.toy.timeline.domain.enumeration.TopicConverter;
 import java.time.LocalDateTime;
-import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Getter
 @Entity
-public class Timeline {
+public class TimelineKeyword {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  @Convert(converter = TopicConverter.class)
-  private Topic topic;
-
-  private String title;
-
-  private String subTitle;
-
-  private String content;
-
-  private String topicStartYyyy;
-
-  private String topicStartMm;
-
-  private String topicStartDd;
-
-  private String topicEndYyyy;
-
-  private String topicEndMm;
-
-  private String topicEndDd;
+  private String keyword;
 
   private String creId;
 
@@ -51,5 +28,6 @@ public class Timeline {
 
   @UpdateTimestamp
   private LocalDateTime updDtt;
+
 
 }
