f26343c36b2973291b019c367cd055d38a921684
rhs <ar.ryuhs@gmail.com>
2020-03-02 23:20:06 +0900

    하위 topic 조회 테스트

--- rhs-timeline-api/src/test/java/io/toy/topic/repository/TopicRepositoryTest.java
+++ rhs-timeline-api/src/test/java/io/toy/topic/repository/TopicRepositoryTest.java
@@ -3,6 +3,7 @@ package io.toy.topic.repository;
 import static org.junit.jupiter.api.Assertions.*;
 
 import io.toy.topic.domain.Topic;
+import java.util.Optional;
 import javassist.NotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -57,6 +58,35 @@ class TopicRepositoryTest {
         .orElseThrow(() -> new NotFoundException("조회된 내역이 없습니다."));
 
     assertEquals(topic.getName(), rsTopic.getName());
+
+  }
+
+  @Test
+  void 하위_TOPIC_조회() throws NotFoundException {
+
+    Topic parent = Topic.builder()
+        .name("책")
+        .build()
+        ;
+
+    testEntityManager.persist(parent);
+
+    Topic rsParent = testEntityManager.find(Topic.class, 1L);
+
+    Topic topic = Topic.builder()
+        .name("추리소설")
+        .parent(rsParent)
+        .build()
+        ;
+
+    testEntityManager.persist(topic);
+
+    Topic rsTopic = topicRepository.findById(2L)
+        .orElseThrow(()->new NotFoundException("조회된 내역이 없습니다"));
+
+
+    assertEquals(topic.getName(), rsTopic.getName());
+    assertEquals(topic.getParent().getName(), rsTopic.getParent().getName());
 
   }
 
