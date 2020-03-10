960f044af603f143800a0875084ec6fe828f95cd
rhs <ar.ryuhs@gmail.com>
2020-03-10 23:52:23 +0900

    baseic repository

---
+++ rhs-timeline-api/src/main/java/io/toy/keyword/repository/KeywordRepository.java
@@ -0,0 +1,8 @@
+package io.toy.keyword.repository;
+
+import io.toy.keyword.domain.Keyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+
+}
