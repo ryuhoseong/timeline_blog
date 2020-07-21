c51054f230f9d9147be4718efcc5c1d63aa8a124
rhs <ar.ryuhs@gmail.com>
2020-07-21 21:53:01 +0900

    mapping 주소

--- rhs-timeline-api/src/main/java/io/toy/topic/controller/TopicRestController.java
+++ rhs-timeline-api/src/main/java/io/toy/topic/controller/TopicRestController.java
@@ -3,16 +3,18 @@ package io.toy.topic.controller;
 import io.toy.topic.service.TopicService;
 import io.toy.topic.service.dto.TopicDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("timeline/v1")
 @RestController
 public class TopicRestController {
 
   private final TopicService topicService;
 
+  @PostMapping("/topic")
   public void save(TopicDto.Create create) {
 
     topicService.save(create);
